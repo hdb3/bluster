5fcc5c6e6877a289f56b5dd78cbfec5ddfeee758
Nicholas Hart <nic@hdb3.net>
2019-01-16 19:37:30 +0000

    WiP

--- Core.hs
+++ Core.hs
@@ -1,30 +1,46 @@
+{-# LANGUAGE FlexibleInstances,DeriveGeneric #-}
 module Core where
 
 import Data.Word
 import Data.Hashable 
 import Data.List(sort)
+import GHC.Generics(Generic)
 
-newtype Prefix = Prefix { fromPrefix :: Int } deriving (Eq,Ord,Show)
-newtype GroupHash = GroupHash { fromGroupHash :: Int } deriving (Eq,Ord,Show)
---newtype ClusterHash = ClusterHash { fromClusterHash :: Word64 }
+newtype Prefix = Prefix { fromPrefix :: Int } deriving (Eq,Ord,Show,Generic)
+newtype Hash = Hash { fromHash :: Int } deriving (Eq,Ord,Show,Generic)
 type PrefixList = [Prefix]
-groupHash :: PrefixList -> GroupHash
-groupHash pfxs = GroupHash $ Data.Hashable.hash $ sort $ map fromPrefix pfxs
 
-data Cluster = Cluster { clPrefixes :: PrefixList -- redundant since the prefix lisst is also contained in the Basic groups
+data Cluster = Cluster { clHash :: Hash
+                       , clPrefixes :: PrefixList -- redundant since the prefix list is also contained in the Basic groups
                        , clCompositeGroups :: [CompositeGroup]
                        , clBasicGroups :: [ BasicGroup ]
                        } deriving Show
 
-data BasicGroup = BasicGroup { bgHash :: GroupHash , basicPrefixes :: PrefixList } deriving Show
+data BasicGroup = BasicGroup { bgHash :: Hash , basicPrefixes :: PrefixList } deriving (Show,Ord,Generic)
+
 instance Eq BasicGroup where
     (==) bg1 bg2 = (bgHash bg1 == bgHash bg2)
 
-data CompositeGroup = CompositeGroup { cgHash :: GroupHash , compositeGroups :: [ BasicGroup ] } deriving Show
+data CompositeGroup = CompositeGroup { cgHash :: Hash , compositeGroups :: [ BasicGroup ] } deriving (Show,Generic)
 
-newCluster = Cluster [] [] []
-newBasicGroup :: PrefixList -> BasicGroup
-newBasicGroup pl = BasicGroup (groupHash pl) pl
+instance {-# INCOHERENT #-} Hashable [Prefix] where
+    hashWithSalt s pl = hashWithSalt s $ sort $ map fromPrefix pl
 
-newCompositeGroup :: [BasicGroup] -> CompositeGroup
-newCompositeGroup bgs = CompositeGroup (groupHash (concatMap basicPrefixes bgs)) bgs
+instance Hashable Prefix
+instance Hashable CompositeGroup
+instance Hashable BasicGroup
+instance Hashable Hash
+
+mkBasicGroup :: PrefixList -> BasicGroup
+mkBasicGroup pl = BasicGroup (Hash $ Data.Hashable.hash $ sort pl) (sort pl)
+
+mkCompositeGroup :: [BasicGroup] -> CompositeGroup
+mkCompositeGroup bgs = CompositeGroup (Hash $ Data.Hashable.hash (sort bgs)) (sort bgs)
+
+mkCluster :: PrefixList -> [CompositeGroup] -> [ BasicGroup ] -> Cluster
+mkCluster a b c = Cluster (Hash $ Data.Hashable.hash (a,b,c)) a b c
+
+emptyCluster = mkCluster [] [] [] 
+
+mergeClusters :: [Cluster] -> Cluster
+mergeClusters = foldl (\(Cluster _ acca accb accc) (Cluster _ xa xb xc) -> (mkCluster (acca++xa) (accb++xb) (accc++xc))) emptyCluster
--- Operations.hs
+++ Operations.hs
@@ -12,15 +12,17 @@ data State = State { clusterList :: ClusterList
 
 newState = State newClusterList newGroupRib newPrefixRib
 
-insertGroup :: PrefixList -> State -> State
+ribUpdate :: PrefixList -> State -> State
 --insertGroup _ a = a
-insertGroup pl s = if present then s else State{..} where
+--ribUpdate pl s = if present then s else State{..} where
+ribUpdate pl s = if present then (error "trying to insert an existing group") else State{..} where
     hash = groupHash pl
     present = isJust (Map.lookup groups hash)
-    clusterMap = getClusterMap pl (prefixRib s)
+    (clusterMap,unmatchedPrefixList) = getClusterMap pl (prefixRib s)
     -- clusterMap = getClusterMap pl prefixes -- using RecordWildCards
-    getClusterMap :: PrefixList -> PrefixRib -> ([(GroupHash,[Prefix]),[Prefix])
-    -- first value is prefixes mapped already, second value is unmapped prefixes
+
+    getClusterMap :: PrefixList -> PrefixRib -> ([(GroupHash,PrefixList),PrefixList)
+    -- first value returns prefixes mapped already, second value is unmapped prefixes
     getClusterMap pl pr = (rollUp matched, sort unmatched) where
         --r = map (\pfx -> (pfx,Map.lookup pfx pr)) pl
         --(matched,unmatched) = foldl (\(m,u) t -> if isNothing (snd t) then (m,t:u) else (t:m,u)) ([],[]) r
@@ -34,22 +36,42 @@ insertGroup pl s = if present then s else State{..} where
     -- the rrturn value is two lists of groups, which together define both the group rib update and the new cluster
     -- the second list contributes to the new group
 
-    -- now we have the affected clusters (and the prefixes involved)
-    -- mtahc up the clusters with their update sets:
-    targetClusters = map (\(hash,pfxs) -> (fromJust $ Map.lookup hash (clusterList s),pfxs)) clusterMap
-    updateCluster :: PrefixList -> (GroupHash,[Prefix]) -> ([ Group ] , [ Group ] )
-    -- cluster update requires first the generation of any new basic groups
-    --     new basic groups are always binary partiotins of existing basic groups
-    --     so the result of this operation is just a change set
-    --     the composite groups are added to the cluster composite list, basic groups to the basic list
-    --     old basic groups are discarded, and old composite groups must be updated where their members are affected by splitting former basic groups
-    updateCluster _ _ = ([],[]) -- TODO....
-    (updates,newGroup) = getClusterMap pl prefixes
-    (axn,bxn) = foldl (\(accb,acca) (a,b) -> (a++acca, b++accb)) ([],[]) updates -- maybe a zippy function does this cleaner?
-    newCompositeGroup = if null newGroup then axx else Basic newGroup : axx
-    newGroups = 
-    newCluster = Cluster 
-    newPrefixList =
+    -- now we have the hashes for the affected clusters matched up with the prefixes involved
+    -- look up the actual cluster values in clusterList and pair up with their update sets:
+    targets :: [(Cluster,PrefixList)]
+    targets = map (\(hash,pfxs) -> (fromJust $ Map.lookup hash (clusterList s),pfxs)) clusterMap
+
+    -- The signature of clusterUpdate is: [(Cluster,PrefixList)] -> (Cluster,[CompositeGroup])
+    -- Note: the output [CompositeGroup] update list is a subset of the CompositeGroups in the new Cluster, so a suboptimal strategy is to update every CompositeGroup present in the new Cluster.
+    updateCluster :: (Cluster,PrefixList) -> (Cluster,[CompositeGroup])
+    updateCluster _ = (emptyCluster,[]) -- todo
+        where
+
+        updateBasicGroups :: [BasicGroup] -> [Prefix] -> ([BasicGroup],[(BasicGroup, BasicGroup, BasicGroup)])
+        updateBasicGroups _ _ = ([],[])
+
+        updateCompositeGroup :: [(BasicGroup,BasicGroup,BasicGroup)] -> CompositeGroup -> CompositeGroup
+        updateCompositeGroup _ _ = newCompositeGroup []
+
+        updateCompositeGroups :: [(BasicGroup,BasicGroup,BasicGroup)] -> [CompositeGroup] -> [CompositeGroup]
+
+    updateClusters :: [(Cluster,PrefixList)] -> (Cluster,[CompositeGroup])
+    updateClusters ax = (mergeClusters cls, concat cgs) where
+        (cls,cgs) = foldl (\(accb,acca) (a,b) -> (a:acca, b:accb)) ([],[]) ax
+
+    -- this marks the completeion of the first stage
+    (tmpCluster,newCompositeGroups) = updateClusters targets
+    markedClusters = map fst targets
+    newBasicGroup = mkBasicGroup unmatchedPrefixList
+
+    -- build the new cluster
+    newCluster = Cluster (unmatchedPrefixList ++ clPrefixes tmpCluster) (newCompositeGroup : clCompositeGroups tmpCluster) (newBasicGroup : clBasicGroups tmpCluster)
+
+    -- now update the RIB state
+    clusterHash
+    clusterList = updateClusterList markedClusters newCluster (clusterList s)
+    groupRib = updateGroupRib newCompositeGroups (groupRib s)
+    prefixRib = updatePrefixRib  (prefixRib s)
 
 displayState :: State -> String
 displayState State{..} = "clusters: " ++ show (length clusters)
