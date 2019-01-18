f7613ce0236aabb304327f854e8a5b0a4d2f4e33
Nicholas Hart <nic@hdb3.net>
2019-01-18 16:40:14 +0000

    basic analysis fraework ready

--- Analysis.hs
+++ Analysis.hs
@@ -24,15 +24,26 @@ groups = elems . groupRib
 analysis :: State -> String
 analysis s = unlines results
     where
-    results = [ analysis1 s, analysis2 s ]
+    results = [ analysis1 s, analysis2 s, analysis3 s ]
 
     analysis1 = partitionOrderAnalysis
-    analysis2 _ = "analysis2"
+    analysis2 = clusterSizeAnalysis
+    analysis3 = clusterGroupSizeAnalysis
 
 partitionOrderAnalysis :: State -> String
 partitionOrderAnalysis State{..} = "partitionOrderAnalysis: " ++ show ( histogram partionSizes )
     where
     partionSizes = map ( fromIntegral . length . clBasicGroups) (elems clusterList)
+
+clusterSizeAnalysis :: State -> String
+clusterSizeAnalysis State{..} = "clusterSizeAnalysis: " ++ show ( histogram clusterSizes )
+    where
+    clusterSizes = map ( fromIntegral . length . concatMap basicPrefixes . clBasicGroups) (elems clusterList)
+
+clusterGroupSizeAnalysis :: State -> String
+clusterGroupSizeAnalysis State{..} = "clusterGroupSizeAnalysis: " ++ show ( histogram clusterSizes )
+    where
+    clusterSizes = map ( fromIntegral . length . clCompositeGroups) (elems clusterList)
 
 histogram :: [Int] -> [(Int, Int)]
 histogram = map countAndTell . group . sort
