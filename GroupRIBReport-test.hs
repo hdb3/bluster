538738b8fa7db186adff703d997b814a4615484d
Nicholas Hart <nic@hdb3.net>
2019-01-20 10:33:19 +0000

    made +groupRIBReport String not IO ()

--- GroupRIBReport-test.hs
+++ GroupRIBReport-test.hs
@@ -5,6 +5,6 @@ import GroupRIBReport
 main :: IO ()
 main = do
    putStrLn "Operations-test"
-   groupRIBReport [[1,2] , [1,2,3] , [1,2,3,4] , [5] ]
+   putStrLn $ groupRIBReport [[1,2] , [1,2,3] , [1,2,3,4] , [5] ]
    putStrLn "done"
 
--- GroupRIBReport.hs
+++ GroupRIBReport.hs
@@ -5,22 +5,21 @@ import Operations
 import Consistency
 import Analysis
 
-groupRIBReport :: [[Int]] -> IO ()
-groupRIBReport pfxs = do
-    putStrLn "groupRIBReport"
-    let st = mapRu newState pfxs
-    putStrLn $ "input prefix groups: " ++ show (length pfxs)
-    putStrLn $ yellow $ displayState st
-    maybe (putStrLn $ green "Consistency check pass") (\s -> putStrLn $ red $ "Consistency check fail: " ++ s) (consistency st)
-    putStrLn $ yellow $ "analysis\n" ++ analysis st
-    putStrLn "GroupRIBReport done"
+groupRIBReport :: [[Int]] -> String
+groupRIBReport pfxs =
+    "groupRIBReport"
+    ++ "\ninput prefix groups: " ++ show (length pfxs)
+    ++ "\n" ++ ( yellow $ displayState st)
+    ++ "\n" ++ ( maybe (green "Consistency check pass") (\s -> red $ "Consistency check fail: " ++ s) (consistency st) )
+    ++ "\n" ++ ( yellow $ "analysis\n" ++ analysis st)
+    ++ "\nGroupRIBReport done"
 
     where
 
+    st = mapRu newState pfxs
     yellow s = "\x001b[33m" ++ s ++ "\x001b[0m"
     green s = "\x001b[32m" ++ s ++ "\x001b[0m"
     red   s = "\x001b[31m" ++ s ++ "\x001b[0m"
 
     ru pl = ribUpdate (map Prefix pl)
     mapRu = foldl (flip ru)
-
